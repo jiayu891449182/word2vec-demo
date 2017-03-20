25b636277bd07090ab0da50245c90c507022b985
Chao Wu <wuchao@zintow.com>
2017-03-20 23:27:26 +0800

    init

---
+++ src/main/java/com/test/nlp/Word2VecTest.scala
@@ -0,0 +1,18 @@
+package com.test.nlp
+
+import org.apache.spark.mllib.feature.Word2Vec
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Word2VecTest {
+
+  def main(args : Array[String]) : Unit = {
+    val sc : SparkContext = new SparkContext(new SparkConf)
+    val input = sc.textFile(args(0))
+      .map(line => line.split(" ").toSeq)
+    val word2vec = new Word2Vec()
+    word2vec.setSeed(100)
+    val word2VecModel = word2vec.fit(input)
+    val synonyms = word2VecModel.findSynonyms("北京", 5)
+    synonyms
+  }
+}
